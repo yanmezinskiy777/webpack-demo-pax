373adda9b963595215203d83334f52997f662081
yan.mezinskiy <yan.mezinskiy@sm-analytics.com>
2022-07-07 18:57:12 +0300

    mode

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: "./src/app/index.js",
-  mode: "development",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
